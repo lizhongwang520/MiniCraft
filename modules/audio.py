b231b19da483f7ed50be2d157b79cc3c792c53e7
Jason <yhorg@hotmail.com>
2017-07-17 19:20:11 +0800

    Revise audio playing logic.

--- modules/audio.py
+++ modules/audio.py
@@ -5,20 +5,16 @@ class Audio(object):
     def __init__(self, src):
         self.src = src
         self.player = None
-        self.settings = {
-            'recycle': False
-        }
-
-    def on_eos(self):
-        if self.settings['recycle']:
-            self.player.play()
 
     def play(self, recycle=False):
-        self.settings['recycle'] = recycle
-
-        music = pyglet.media.load(self.src, streaming=False)
-        self.player = music.play()
-
+        # Create a player
+        self.player = pyglet.media.Player()
+        # Load a sound file
+        sound = pyglet.media.load(self.src, streaming=False)
+        # Resource enqueue
+        self.player.queue(source=sound)
         # Events
-        self.player.on_eos = self.on_eos
+        self.player.on_eos = pyglet.media.SourceGroup.loop
+        # Play
+        self.player.play()
 
